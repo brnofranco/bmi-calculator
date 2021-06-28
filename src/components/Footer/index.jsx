aaf142a545c1a8c556a7b51dc687b49ea1dd926f
Bruno Franco <11.brunofm@gmail.com>
2021-06-28 19:55:00 -0300

    Remoção do Footer

--- src/components/Footer/index.jsx
+++
@@ -1,7 +0,0 @@
-import './styles.css';
-
-export const Footer = () => (
-    <>
-        <p className="footer"> bmi.calc </p>
-    </>
-)
--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './styles.css';
-import { Footer } from '../../components/Footer';
 import { Form } from '../../components/Form';
 import  { Header } from '../../components/Header';
 import { Table } from '../../components/Table';
